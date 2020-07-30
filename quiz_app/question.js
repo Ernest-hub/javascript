0aa5aeede62b9faefd16ebcd961ff46905858213
Ernest-hub <55037492+Ernest-hub@users.noreply.github.com>
2020-07-30 04:18:47 -0400

    Committing all files after adding

---
+++ quiz_app/question.js
@@ -0,0 +1,44 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charles Babbage was called ?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Which of the folowing is the most powerful type of computer ?",
+	"option1": "Super-micro",
+	"option2": "SUper Conductor",
+	"option3": "Super computer",
+	"option4": "Megaframe",
+	"answer": "3"
+}, {
+	"question": "which is a single integrated circuit ?",
+	"option1": "Gate",
+	"option2": "Mother Board",
+	"option3": "Chip",
+	"option4": "CPU",
+	"answer": "1"
+}, {
+	"question": "C is ?",
+	"option1": "A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Web pages a written using ?",
+	"option1": "FTP",
+	"option2": "HTTP",
+	"option3": "HTML",
+	"option4": "URL",
+	"answer": "3"
+}, {
+	"question": "UNIVAC is an example of ?",
+	"option1": "First generation computer",
+	"option2": "Second generation computer",
+	"option3": "Third generation computer",
+	"option4": "Fourth generation computer",
+	"answer": "1"
+
+}];
